4e2562628617299fcdf3f2d56635ca2eea85e1f6
swordfish <swordfishmail@gmail.com>
2014-11-22 14:35:46 +0100

    Initial Commit

---
+++ LemonadeStandAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LemonadeStandAssignment
+//
+//  Created by iMac on 22/11/14.
+//  Copyright (c) 2014 mrodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
